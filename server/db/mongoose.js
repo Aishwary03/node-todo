d34f213c066909b162bf28ba31ac369b26f5b600
Aishwary03 <agrawal.aishwary@tcs.com>
2018-06-10 15:40:45 +0530

    heroku changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://Aishwary03:mongodb123@ds141720.mlab.com:41720/todoapp" || "mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
 
 module.exports = {mongoose};
